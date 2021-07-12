1d9d6e3d61ba1b30191592ff401d03d9e6ab0cdb
Gleb <demonxklimenko@mail.ru>
2021-07-12 22:30:37 +0300

    Test

--- src/test/java/ru/netology/WebInterfaceTest.java
+++ src/test/java/ru/netology/WebInterfaceTest.java
@@ -5,6 +5,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
@@ -19,25 +20,33 @@ public class WebInterfaceTest {
     }
 
     @BeforeEach
-    void setUp(){
-        driver = new ChromeDriver();
+    public void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--headless");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
-    void tearDown(){
-        driver.quit();
-        driver = null;
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
     void shouldSubmitRequest() {
-    open("http://localhost:9999");
-    $("[data-test-id=name] input").setValue("Олег Григорий");
-    $("[data-test-id=phone] input").setValue("+79873451204");
-    $("[data-test-id=agreement]").click();
-    $("[type=button]").click();
-    $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно " +
-            "отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-}
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Олег Григорий");
+        $("[data-test-id=phone] input").setValue("+79873451204");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно " +
+                "отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
 
